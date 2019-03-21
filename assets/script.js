baf82aa7d9f21894602bcfaaf44aa42a0b6208be
Filipbagen <filip.bagen@gmail.com>
2019-03-21 14:15:35 +0100

    Updated javascript
    Animated text and chhanged style

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,67 @@
-let lastWord
 const words = [
-  'Prova osttoasten idag!',
-  'Kan du inte ta cykeln imorgon till skolan?',
-  'Ge någon en extra kram!',
-  'Köp svenska livsmedel.'
+  'Prova ost-tosten idag!',
+  'Ta cykeln till skolan imorgon!',
+  'Köp svenska livsmedel.',
+  'Åk kollektivt istället för att ta bilen!',
+  'Stäng av vattenkranen när du borstar tänderna.',
+  'Ta en tur och panta de där burkarna/flaskorna.',
+  'Skänk/sälj tröjan du inte använder'
+
 ]
+
+let number = 0
+const txt = document.querySelector('#txt')
+const txt2 = document.querySelector('#txt2')
+
+const evenOrOdd = () => {
+  number++
+  return number
+}
 
 const init = () => {
   displayWord()
 }
 
-const displayWord = () => {
-  document.querySelector('#call').textContent = getWord()
-}
 
-const getWord = () => {
-  const getWord = words[randomNum()]
-  console.log(getWord)
-  return getWord
+console.log(evenOrOdd())
+
+
+
+
+
+
+const displayWord = () => {
+  const word = words[randomNum()]
+  txt.textContent = word
+
+  const word2 = words[randomNum()]
+  txt2.textContent = word2
 }
 
 const randomNum = () => {
-  let num = Math.floor(Math.random() * words.length)
-  const word = words[num]
-
-  if (word === lastWord) {
-    num = randomNum()
-  }
-
-  lastWord = word
+  const num = Math.floor(Math.random() * words.length)
   return num
+}
+
+const changeText = () => {
+
+  if (evenOrOdd() % 2 === 0) {
+    console.log('number is even')
+    txt.classList.remove('in')
+    txt.classList.add('out')
+
+    txt2.textContent = words[randomNum()]
+    txt2.classList.remove('out')
+    txt2.classList.add('in')
+
+  } else {
+    console.log('number is odd')
+    txt2.classList.remove('in')
+    txt2.style.visibility = 'visible'
+    txt2.classList.add('out')
+
+    txt.textContent = words[randomNum()]
+    txt.classList.remove('out')
+    txt.classList.add('in')
+  }
 }
